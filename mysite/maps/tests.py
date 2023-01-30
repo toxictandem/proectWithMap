22eb288e4ef0cc3a904df8d873d67c46317be101
admin <kalyuzh.ka@yandex.ru>
2023-01-30 11:00:29 +0300

    try 2

---
+++ mysite/maps/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your tests here.
+
+def shablon(request):
+    return render('-----------')
