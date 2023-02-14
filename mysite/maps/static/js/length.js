7fa8dfab4b359a5bfc79558b303f9c4649efcebd
admin <kalyuzh.ka@yandex.ru>
2023-02-14 15:51:40 +0300

    9

--- mysite/maps/static/js/length.js
+++ mysite/maps/static/js/length.js
@@ -1,5 +1,4 @@
-
-
+window.superTempDuration = 808;
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
         center: [55.753994, 37.622093],
@@ -18,7 +17,9 @@ ymaps.ready(function () {
     tempNeedTime = 900;
     bestLength = 1;
     bestArray = [addressStart + 1];
-    generator(1, bestArray, 1, 0, 900);
+    bestPointArray = [1];
+    generator(1, bestArray, 1, 0, 900, [1]);
+    console.log(bestPointArray);
 
 
 
@@ -44,6 +45,7 @@ ymaps.ready(function () {
         console.log("Длина: " + activeRoute.properties.get("distance").text);
         console.log("Время прохождения: " + activeRoute.properties.get("duration").text);
         console.log(activeRoute.properties.get("duration").value);
+        console.log("Точки маршрута: " + bestArray);
         // Для автомобильных маршрутов можно вывести
         // информацию о перекрытых участках.
         if (activeRoute.properties.get("blocked")) {
@@ -53,37 +55,45 @@ ymaps.ready(function () {
     // Добавление маршрута на карту.
     myMap.geoObjects.add(multiRoute);
 
-    function generator(pathLength, pathArray, lastPoint, pathDuration, needTime) {
-        if (pathDuration < needTime) {
-            if (needTime - pathDuration <= needTime / 15 && pathLength > bestLength) {
-                bestLength = pathLength;
-                bestArray = pathArray;
-            }
-        } else {
-            if (pathDuration - needTime <= needTime / 15 && pathLength > bestLength) {
-                bestLength = pathLength;
-                bestArray = pathArray;
-            }
-        }
-        if (pathDuration - needTime > needTime / 15) {
-            return 0;
-        }
-        for (var i = lastPoint + 1; i < 20; i++) {
-            var tempArray = pathArray;
-            var tempDuration = -1;
-            tempArray.push(addressStart + i);
-            var tempRoute = new ymaps.multiRouter.MultiRoute({
-                referencePoints: tempArray,
+
+    function getPathDuration(pathArrayTemp) {
+        var superTempRoute = new ymaps.multiRouter.MultiRoute({
+                referencePoints: pathArrayTemp,
                 params: {
                     routingMode: "pedestrian"
                 }
             });
-            multiRoute.model.events.add('requestsuccess', function() {
-                var activeRouteTemp = tempRoute.getActiveRoute();
-                tempDuration = activeRouteTemp.properties.get("duration").value;
+            superTempRoute.model.events.add('requestsuccess', function() {
+                var activeRouteSuperTemp = superTempRoute.getActiveRoute();
+                window.superTempDuration = activeRouteSuperTemp.properties.get("duration").value;
+                console.log("!!! ", superTempDuration);
+                //return (superTempDuration);
             });
-            generator(pathLength + 1, tempArray, i, tempDuration, needTime);
+            console.log("??? ", superTempDuration);
+            return superTempDuration;
+    }
+
+    function generator(pathLength, pathArray, lastPoint, pathDuration, needTime, pointArray) {
+        console.log(pathLength, pathDuration, needTime);
+        if (pathDuration < needTime) {
+            if (pathDuration <= needTime && pathLength > bestLength) {
+                bestLength = pathLength;
+                bestArray = pathArray;
+                bestPointArray = pointArray;
+            }
+        } else {return 0;}
+
+        for (var i = lastPoint + 1; i < 5; i++) {
+            var tempArray = pathArray;
+            var tempDuration = 808;
+            var tempPointArray = pointArray;
+            tempArray.push(addressStart + i);
+            tempDuration = getPathDuration(tempArray);
+            tempPointArray.push(i)
+            generator(pathLength + 1, tempArray, i, tempDuration, needTime, tempPointArray);
         }
+        return 0;
     }
 });
-
+//aboba
+//sus
